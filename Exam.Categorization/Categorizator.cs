80de86f06eb9e1185c843986aed4e66345317f0b
Gergana Georgieva <gerganagergana1@gmail.com>
2023-02-04 20:44:40 +0200

    Implemented Dictionaries

--- Exam.Categorization/Categorizator.cs
+++ Exam.Categorization/Categorizator.cs
@@ -7,163 +7,10 @@ namespace Exam.Categorization
 {
     public class Categorizator : ICategorizator
     {
-        public class Tree<T>
-        {
-            public T value;
-            private Tree<T> parent;
-            public readonly List<Tree<T>> children;
-
-            public Tree(T value)
-            {
-                this.value = value;
-                this.children = new List<Tree<T>>();
-            }
-
-            public Tree(T value, params Tree<T>[] children)
-                : this(value)
-            {
-                foreach (var child in children)
-                {
-                    child.parent = this;
-                    this.children.Add(child);
-                }
-            }
-
-            //public List<Tree<T>> ChildrenOfNode()
-            //{
-            //    return this.children;
-            //}
-
-
-            public void AddChild(T parentKey, Tree<T> child)
-            {
-                var parentNode = this.FindNodeWithBfs(parentKey);
-                if (parentNode != null)
-                {
-                    parentNode.children.Add(child);
-                    child.parent = parentNode;
-                }
-                else
-                {
-                    throw new ArgumentNullException();
-                }
-            }
-
-            public Tree<T> FindNodeWithBfs(T parentKey)
-            {
-                var queue = new Queue<Tree<T>>();
-                queue.Enqueue(this);
-
-                while (queue.Count > 0)
-                {
-                    var subtree = queue.Dequeue();
-
-                    if (subtree.value.Equals(parentKey))
-                    {
-                        return subtree;
-                    }
-
-                    foreach (var child in subtree.children)
-                    {
-                        queue.Enqueue(child);
-                    }
-                }
-
-                return null;
-            }
-
-            public IEnumerable<T> OrderBfs()
-            {
-                var result = new List<T>();
-
-                var queue = new Queue<Tree<T>>();
-                queue.Enqueue(this);
-
-                while (queue.Count > 0)
-                {
-                    var subtree = queue.Dequeue();
-                    result.Add(subtree.value);
-
-                    foreach (var child in subtree.children)
-                    {
-                        queue.Enqueue(child);
-                    }
-                }
-
-                return result;
-            }
-
-            public IEnumerable<T> OrderDfs()
-            {
-                var result = new Stack<T>();
-                var stack = new Stack<Tree<T>>();
-                stack.Push(this);
-
-                while (stack.Count > 0)
-                {
-                    var node = stack.Pop();
-
-                    foreach (var child in node.children)
-                    {
-                        stack.Push(child);
-                    }
-
-                    result.Push(node.value);
-                }
-
-                return result;
-            }
-
-            public void RemoveNode(T nodeKey)
-            {
-                var toBeDeleted = FindNodeWithBfs(nodeKey);
-
-                if (toBeDeleted is null)
-                {
-                    throw new ArgumentNullException();
-                }
-
-                var parentNode = toBeDeleted.parent;
-
-                if (parentNode is null)
-                {
-                    throw new ArgumentException();
-                }
-
-                parentNode.children.Remove(toBeDeleted);
-            }
-
-            public void Swap(T firstKey, T secondKey)
-            {
-                var firstNode = this.FindNodeWithBfs(firstKey);
-                var secondNode = this.FindNodeWithBfs(secondKey);
-
-                if (firstNode is null || secondNode is null)
-                {
-                    throw new ArgumentNullException();
-                }
-
-                var firstParent = firstNode.parent;
-                var secondParent = secondNode.parent;
-
-                if (firstParent is null || secondParent is null)
-                {
-                    throw new ArgumentException();
-                }
-
-                var indexOfFirstChild = firstParent.children.IndexOf(firstNode);
-                var indexOfSecondChild = secondParent.children.IndexOf(secondNode);
-
-                firstParent.children[indexOfFirstChild] = secondNode;
-                secondNode.parent = firstParent;
-
-                secondParent.children[indexOfSecondChild] = firstNode;
-                firstNode.parent = secondParent;
-            }
-        }
-
-        private Tree<Category> tree;
-        private Dictionary<string, Tree<Category>> categoriesById = new Dictionary<string, Tree<Category>>();
+        Dictionary<string, Category> categoriesById = new Dictionary<string, Category>();
+        Dictionary<string, Dictionary<string, Category>> categoriesByParentId = new Dictionary<string, Dictionary<string, Category>>();
+        Dictionary<string, string> parentsByCategoryId = new Dictionary<string, string>();
+        Dictionary<string, int> categoriesDepths = new Dictionary<string, int>();
 
         public void AddCategory(Category category)
         {
@@ -172,7 +19,7 @@ namespace Exam.Categorization
                 throw new ArgumentException();
             }
 
-            categoriesById.Add(category.Id, new Tree<Category>(category));
+            categoriesById.Add(category.Id, category);
         }
 
         public void AssignParent(string childCategoryId, string parentCategoryId)
--- Exam.Categorization/Category.cs
+++ Exam.Categorization/Category.cs
@@ -9,15 +9,12 @@ namespace Exam.Categorization
         public string Name { get; set; }
 
         public string Description { get; set; }
-        //public List<Category> Children { get; set; }
-        //public Category Parent { get; set; }
 
         public Category(string id, string name, string description)
         {
             Id = id;
             Name = name;
             Description = description;
-            //Children = new List<Category>();
         }
     }
 }
